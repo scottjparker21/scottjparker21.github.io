9eed982a1f401f7e0738a745d7a230dd6445cc05
scottjparker21 <scottjparker21@gmail.com>
2015-11-09 17:14:21 -0600

    quick fix

--- js/posts.js
+++ js/posts.js
@@ -1,18 +1,13 @@
 
    "use strict";
   function getposts(){
-
-        var entries = [];
-
+        // var entries = [];
       $.getJSON("entries.json", function( data ) {
           $.each(data.posts, function(key, value) {
-                entries.push(value.t + "<br>" + value.h + "<br>" + value.c + "<br>");
-
-              console.log(value.t);
-              console.log(value.h);
-              console.log(value.c);
-             
+              // entries.push(value.t + "<br>" + value.h + "<br>" + value.c + "<br>");
+              $('#posts').append(value.t + "<br>");
+              $('#posts').append(value.h  + "<br>");
+              $('#posts').append(value.c  + "<br>");
           });
-           $('posts').html();
       });
   }
