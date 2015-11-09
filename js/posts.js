5c8070bd4875492ef185e57cdd440f5b19ff461b
scottjparker21 <scottjparker21@gmail.com>
2015-11-09 17:07:13 -0600

    function outsourced to js file.

--- js/posts.js
+++ js/posts.js
@@ -1,9 +1,18 @@
 
-                      function getposts(){
-                $.get("entries.json", function( data ) {
-                    $.each(data, function(key, value) {
-                        console.log(value['text']);
-                    });
-                });
-            }
-       
+   "use strict";
+  function getposts(){
+
+        var entries = [];
+
+      $.getJSON("entries.json", function( data ) {
+          $.each(data.posts, function(key, value) {
+                entries.push(value.t + "<br>" + value.h + "<br>" + value.c + "<br>");
+
+              console.log(value.t);
+              console.log(value.h);
+              console.log(value.c);
+             
+          });
+           $('posts').html();
+      });
+  }
