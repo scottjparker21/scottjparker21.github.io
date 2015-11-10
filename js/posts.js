10df6734aeaac7480bd3b7e44a6f9ce8a90511a4
scottjparker21 <scottjparker21@gmail.com>
2015-11-09 21:26:48 -0600

    try class fix

--- js/posts.js
+++ js/posts.js
@@ -5,7 +5,7 @@
       $.getJSON("entries.json", function( data ) {
           $.each(data.posts, function(key, value) {
               // entries.push(value.t + "<br>" + value.h + "<br>" + value.c + "<br>");
-              $('#posts').append('&nbsp' + '<p id="date">' + value.t + '</p>' + "<br>");
+              $('#posts').append('<p id="date">' + value.t + '</p>' + "<br>");
               $('#posts').append('<p id="title">' + value.h  + '</p>' + "<br>");
               $('#posts').append('<p id="postcontent">' + value.c  + '</p>' + "<br>");
           });
