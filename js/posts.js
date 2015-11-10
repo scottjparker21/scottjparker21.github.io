d1b438cc0d5b7a2041a27bd115fc541ab8a4654d
scottjparker21 <scottjparker21@gmail.com>
2015-11-09 21:12:12 -0600

    try class fix

--- js/posts.js
+++ js/posts.js
@@ -6,8 +6,8 @@
           $.each(data.posts, function(key, value) {
               // entries.push(value.t + "<br>" + value.h + "<br>" + value.c + "<br>");
               $('#posts').append('<p id="date">' + value.t + '</p>' + "<br>");
-              $('#posts').append(value.h  + "<br>");
-              $('#posts').append(value.c  + "<br>");
+              $('#posts').append('<p id="title">' + value.h  + '</p>' + "<br>");
+              $('#posts').append('<p id="postcontent">' + value.c  + '</p>' + "<br>");
           });
       });
   }
