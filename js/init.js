06f1371aca1423ad4c78654806907ebab6689452
scottjparker21 <scottjparker21@gmail.com>
2017-08-22 00:02:12 -0500

    added to portfolio page

--- js/init.js
+++ js/init.js
@@ -6,3 +6,16 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+ $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+       
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+
+  $(document).ready(function(){
+      $('.slider').slider();
+    });
